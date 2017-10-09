91d49bc405c29fc4a29e38672e992c4b0d93d160
Logan Pugh <logan.pugh@austintexas.gov>
2017-10-09 16:33:55 -0500

    Fix default report directory being a level deeper than it should

--- ags_service_publisher/reporters/__init__.py
+++ ags_service_publisher/reporters/__init__.py
@@ -2,7 +2,7 @@ import os
 
 default_report_dir = os.getenv(
     'AGS_SERVICE_PUBLISHER_REPORT_DIR',
-    os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'reports'))
+    os.path.abspath(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'reports'))
 )
 
 from dataset_geometry_statistics_reporter import DatasetGeometryStatisticsReporter
