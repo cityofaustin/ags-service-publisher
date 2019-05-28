30c5bb13147c0623eef538f34f7aa917c005a68f
Logan Pugh <logan.pugh@austintexas.gov>
2019-05-28 11:36:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='ags_service_publisher',
-    version='1.0.0',
+    version='1.2.0',
     packages=['ags_service_publisher', 'ags_service_publisher.reporters'],
     install_requires=[
         'requests',
         'PyYAML'
     ],
-    url='https://github.austintexas.gov/ctm-egs/ags-service-publisher',
+    url='https://github.com/cityofaustin/ags-service-publisher',
     license='',
     author='Logan Pugh',
     author_email='logan.pugh@austintexas.gov',
