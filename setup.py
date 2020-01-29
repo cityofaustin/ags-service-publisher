ed5bfc342757c734ac92dde538f19bf0183bdd41
Logan Pugh <logan.pugh@austintexas.gov>
2020-01-29 17:14:20 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ags_service_publisher',
-    version='1.2.1',
+    version='1.3.0',
     packages=['ags_service_publisher', 'ags_service_publisher.reporters'],
     install_requires=[
         'requests',
